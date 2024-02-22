6a9bd140e68fa64233c5d031de50746b77dd7015
ethandabbs2 <ethandabbs2@yahoo.com>
2024-02-22 16:16:00 -0600

    graded assignment

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -2,17 +2,24 @@ package org.launchcode.techjobs.persistent.models;
 
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
-
+ @MappedSuperclass
 public abstract class AbstractEntity {
 
-    private int id;
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private int id;
 
+     @NotNull
+     @NotBlank
+     @Size(max = 100)
     private String name;
 
     public int getId() {
